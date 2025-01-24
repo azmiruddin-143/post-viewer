80ddcff76a67370af659c29534750933f1254c8d
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-01-24 16:51:24 +0600

    next config file update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,25 @@
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ["lh3.googleusercontent.com"],
+//     },
+// };
+
+// export default nextConfig;
 
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["lh3.googleusercontent.com"],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '',
+            },
+        ],
     },
 };
+
 
 export default nextConfig;
