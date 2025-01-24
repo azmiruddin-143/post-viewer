658ff6b572fbcc90c18908e45f58645573256b35
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-01-24 16:25:17 +0600

    profile page update

--- src/lib/getUser.js
+++
@@ -1,7 +0,0 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export default async function getUser() {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
-    return user;
-}
---
+++ src/lib/useAuth.js
@@ -0,0 +1,8 @@
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const useAuth = () => {
+  return getKindeServerSession();
+};
+
+export default useAuth;
